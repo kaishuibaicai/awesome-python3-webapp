64fb3cfef56f0820759ee98b95f8fe52ace8d185
kaishuibaicai <a272251416@gmail.com>
2017-06-09 23:18:08 +0800

    add annotation to orm.py,but didn't complete.

--- www/orm.py
+++ www/orm.py
@@ -71,18 +71,28 @@ def create_args_string(num):         #这个函数在元类中被引用，作用
 	return ', '.join(L)
 
 
+#父域，可被其它域继承
 class Field(object):                 #定义字段基类，后面各种各样的字段类都集成这个基类
+	#域的初始化，包括属性（列）名，属性（列）的类型，是否主键
+	#default参数允许orm自己填入缺省值，因此具体的使用要看具体的类怎么使用
+	#比如User有一个定义在StringField的id, default就用于存储用户的独立id
+	#再比如created_at的default就用于存储创建时间的浮点表示
 	def __init__(self, name, column_type, primary_key, default):
 		self.name = name                  #字段名
 		self.column_type = column_type    #字段类型
 		self.primary_key = primary_key    #主键
 		self.default = default            #默认值
 	
+	#用于打印信息，依次为类名（域名），属性类型，属性名
 	def __str__(self):
 		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 
+#字符串域
 class StringField(Field):
+	#ddl("data definition languages"),用于定义数据类型
+	#varchar("variable char"), 可变长度的字符串，以下定义中的100表示最长长度，即字符串的可变范围为0~100
+	#(char,为不可变长度字符串，会用空格字符来补齐
 	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
 		super().__init__(name, ddl, primary_key, default)
 
@@ -108,65 +118,113 @@ class TextField(Field):
 
 
 
-
+#这是一个元类，它定义了如何来构造一个类，任何定义了__metaclass__属性或指定了metaclass的都会通过元类定义的构造方法构造类
+#任何继承字Molel的类，都会自动通过ModelMetaclass扫描映射关系，并存储到自身的类属性
 class ModelMetaclass(type):
 	def __new__(cls, name, bases, attrs):
+		# cls: 当前准备创建的类对象，相当于self
+		# name: 类名，比如User继承自Model,当使用该元类创建User类时，name=User
+		# bases: 父类的元组
+		# attrs: 属性（方法）的字典，比如User有__table__,id,等，就作为attrs的keys
+		# 排除Model类本身，因为Model类主要就是用来被继承的，其不存在与数据库表的映射
+
 		if name == 'Model':
 			return type.__new__(cls, name, bases, attrs)
+
+		# 以下是针对"Model"的子类的处理，将被用于子类的创建，metaclass将隐式地被继承
+
+		# 获取表名，若没有定义__table__属性，将类名作为表名，此处用到了or方法
 		tableName = attrs.get('__table__', None) or name
 		logging.info('found model: %s (table: %s)' % (name, tableName))
-		mappings = dict()
-		fields = []
-		primaryKey = None
+		
+		# 获取所有的Field和主键名
+		mappings = dict()     # 用字典来储存类属性与数据库表的列的映射关系
+		fields = []           # 用于保存除主键外的属性
+		primaryKey = None     # 用于保存主键
+
+
+		# 遍历类的属性，找出定义域（如StringField,字符串域）内的值，建立映射关系
+		# k是属性名，v其实是定义域！请看name=StringField(ddl="varchar50")	
 		for k, v in attrs.items():
 			if isinstance(v, Field):
 				logging.info('  found mapping: %s ==> %s' % (k, v))
-				mappings[k] = v
-				if v.primary_key:
-					if primaryKey:
+				mappings[k] = v         # 建立映射关系
+				if v.primary_key:       # 找到主键
+					if primaryKey:　# 若主键已存在，又找到一个主键，将报错，每张表有且仅有一个主键
 						raise StandardError('Duplicate primary key for field: %s' % k)
 					primaryKey = k
 				else:
-					fields.append(k)
-		if noet primaryKey:
+					fields.append(k)# 将非主键的属性加入field列表中
+		if not primaryKey:　　　　　　　　　　　# 没有找到主键也将报错，因为每张表有且仅有一个主键
 			raise StandardError('Primary key not found.')
+
+		# 从类属性中删除已加入映射字典的键，避免重名
 		for k in mapping.keys():
 			attrs.pop(k)
+
+		# 将非主键的属性变形，放入escaped_fields中，方便增删改查语句的书写
 		escaped_fields = list(map(lambda f: '`%s`' % f, fields))
 		attrs['__mappings__'] = mappings    #保存属性和列的映射关系
-		attrs['__table__'] = tableName
+		attrs['__table__'] = tableName      #保存表名
 		attrs['__primary_key__'] = primaryKey    #主键属性名
 		attrs['__fields__'] = fields             #除主键外的属性名
+		# 构造默认的select,insert,update,delete语句，使用？作为占位符
 		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+
+		# 此处利用create_args_string生成的若干个?占位
+		# 插入数据时，要指定属性名，并对应的填入属性值
 		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+
+		# 通过主键查找到记录并更新
 		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+
+		# 通过主键删除
 		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+
 		return type.__new__(cls, name, bases, attrs)
 
+
+# ORM映射基类，继承字dict,通过ModelMetaclass元类来构造类
 class Model(dict, mataclass=ModelMetaclass):
+
+	# 初始化函数，调用其父类(dict)的方法
 	def __init__(self, **kw):
 		super(Model, self).__init__(**kw)
+
+	# 增加__getattr__方法，使获取属性更方便，即可通过"a.b"的形式
 	def __getattr__(self, key):
 		try:
 			return self[key]
 		except KeyError:
 			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+	# 增加__setattr__方法，是设置属性更方便，可通过"a.b=c"的形式
 	def __setattr__(self, key, value):
 		self[key] = value
+
+	# 通过键取值，若值不存在，返回None
 	def getValue(self, key):
 		return getattr(self, key, None)
+
+	# 通过键取值，若值不存在，则返回默认值
 	def getValueOrDefault(self, key):
 		value = getattr(self, key, None)
 		if value is None:
-			field = self.__mappings__[key]
+			field = self.__mappings__[key]  # field是一个定义域！比如FloatField
+			# default这个属性在此除再次发挥作用了！
 			if field.default is not None:
+				# 例子如下：
+				# id的StringField.default=next_id,因此调用该函数生成独立id
+				# FloatField.default=time.time数，因此调用time.time函数返回当前时间
+				# 普通属性的StringField默认为None,因此还是返回None
 				value = field.default() if callable(field.default) else field.default
 				logging.debug('using default value for %s: %s' % (key, str(value)))
+				# 通过default取到值之后再将其作为当前值
 				setattr(self, key, value)
 		return value
 
 
-@classmethod
+@classmethod　　　　　　　　　　# 该装饰器将方法定义为类方法
 async def findAll(cls, where=None, args=None, **kw):
 	' find objects by where clause. '
 	sql = [cls.__select__]
@@ -210,9 +268,11 @@ async def findNumber(cls, selectField, where=None, args=None):
 @classmethod
 async def find(cls, pk):
 	' find object by primary key. '
+	# 之前已将数据库的select操作封装在了select函数中，以下select的参数依次是sql, args, size
 	rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
 	if len(rs) == 0:
 		return None
+	# **表示关键字参数，因为在select函数中，打开的是DictCursor,它会以dict的形式返回结果
 	return cls(**rs[0])
 
 
