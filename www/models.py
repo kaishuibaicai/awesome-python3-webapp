5b13171a2440919da39e1e19c3f0a00744b92767
kaishuibaicai <a272251416@gmail.com>
2017-06-10 16:51:24 +0800

    models.py with annotation

--- www/models.py
+++ www/models.py
@@ -12,19 +12,26 @@ import time, uuid
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
 
+# 用当前时间与随机生成的uuid作为id
 def next_id():
+	# uuid4()以随机方式生成uuid, hex属性将uuid转为32位的16进制数
 	return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 
+# ORM映射，将User映射到数据库users表
 class User(Model):
+	# __table__的值将在创建类时被映射为表名
 	__table__ = 'users'
 
+	# 定义各属性的域，以及是否主键，将在创建类时被映射为数据库表的列
+	# 此处default用于存储每个用于独有的id, next_id将在insert的时候被调用
 	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
 	email = StringField(ddl='varchar(50)')
 	passwd = StringField(ddl='varchar(50)')
 	admin = BooleanField()
 	name = StringField(ddl='varchar(50)')
 	image = StringField(ddl='varchar(500)')
+	# 此处default用于存储创建的时间，在insert的时候被调用
 	created_at = FloatField(default=time.time)
 
 
